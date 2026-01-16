d12069fe84ab60c5b8f7c309b1901fe18336b673
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-15 20:33:01 -0500

    avance

---
+++ 03-js/02-operaciones.js
@@ -0,0 +1,25 @@
+//operaciones o estructuras
+//condicionales y repetitivas
+
+//condicionales, operadores lógicos y de comparación
+let edad = 16;
+
+console.log(edad >= 18);
+//dentro de los parentesis va una EXPRESIÓN a ser evaluada
+if (edad >= 18) { //true
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+let numeroGanador = 7;
+
+//cuando es solo 01 =   estamos asignando un valor
+//cuando es 02 ==   estamos comparando valores (solo valor)
+//cuando es 03 ===  estamos comparando valores y tipos de datos
+//lo recomendable es usar ===
+if (numeroGanador === 7) {
+  console.log("Felicidades, ganaste el premio");
+} else {
+  console.log("Lo siento, sigue intentando");
+}
