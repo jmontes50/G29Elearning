3bbc6793e8c7ede23d4db74da5266ced905d7943
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-15 19:46:14 -0500

    avance

--- 03-js/01-javascript.js
+++ 03-js/01-javascript.js
@@ -1,2 +1,25 @@
 console.log("Hola mundo!!!");
 
+//JS, tiene un tipado dinámico, se tipa al momento de darle valor
+
+//comentarios, variables y tipos de datos
+//Tipos de datos primitivos
+let nombre = "Jorge"; //string
+
+console.log(nombre);
+console.log(typeof nombre)
+
+let numero = 100; //number
+
+console.log(numero);
+console.log(typeof numero)
+
+let estaLloviendo = true; //booleando true/false
+
+console.log(estaLloviendo);
+console.log(typeof estaLloviendo);
+
+let regalo; //undefined - no definido
+console.log(regalo);
+console.log(typeof regalo);
+
