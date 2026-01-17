cf37b7f8145a259d0cad2035d7772d66547c4da5
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-16 19:32:56 -0500

    avance

---
+++ 03-js/05-arrays.js
@@ -0,0 +1,14 @@
+let frutas = ["Kiwi", "Naranja", "Piña", "Durazno"];
+
+console.log(frutas);
+console.table(frutas);
+
+console.log(frutas[0]);
+
+let fruta1 = frutas[0];
+
+let fruta3 = frutas[3];
+
+console.log(fruta3);
+
+console.log(frutas.length); //cuantos items days
