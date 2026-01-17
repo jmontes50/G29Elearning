e3e82a11cfef3d1d3e9748254a9f87b247ae7772
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-16 19:45:32 -0500

    avance

--- 03-js/05-arrays.js
+++ 03-js/05-arrays.js
@@ -12,3 +12,22 @@ let fruta3 = frutas[3];
 console.log(fruta3);
 
 console.log(frutas.length); //cuantos items days
+
+// arrays tiene metodos, vienen con herramientas
+
+//agrega un nuevo item al final
+frutas.push("Platano");
+frutas.push("Maracuya");
+console.log(frutas);
+
+//Eliminar/Remover un item del final
+frutas.pop();
+console.log(frutas);
+
+//agrega un nuevo item al inicio
+frutas.unshift("Chirimoya");
+console.log(frutas);
+
+//remueve un item al inicio
+frutas.shift();
+console.log(frutas);
