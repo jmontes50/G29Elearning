eb4a3997e6a30c294676c83c530daeb7e82f1e69
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-16 22:09:07 -0500

    avance

--- 03-js/08-metodosArrays.js
+++ 03-js/08-metodosArrays.js
@@ -10,3 +10,17 @@ let hacerOperacion = (numero, operacion) => {
 console.log(hacerOperacion(17, sumarCien));
 
 console.log(hacerOperacion(21, (num) => num * 1000));
+//-----------------------------------------------
+//IndexOF
+let frutas = ["Kiwi", "Naranja", "Piña", "Manzana"];
+
+//Me indica el indice al buscar algo
+console.log(frutas.indexOf("Naranja")); //me da su indice
+console.log(frutas.indexOf("Mango")); //da -1 si no existe
+
+//forEach - por cada uno haz "esto"
+frutas.forEach((fru, indice, arregloOriginal) => {
+  console.log(fru, indice, arregloOriginal);
+})
+
+
