3da038ef37cd341ff9959f5a12ba1d1644b34229
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-16 20:16:14 -0500

    avance

--- 03-js/06-objetos.js
+++ 03-js/06-objetos.js
@@ -14,7 +14,27 @@ console.log(gato);
 //nombreObj.nombreProp //notación de punto
 console.log(gato.nombre);
 console.log(gato.color);
+
 //en caso de escribir una propiedad incorrecta, dará undefined
 console.log(gato.genero);
 
+//Notación de corchetes para objetos
+console.log(gato["esAmigable"]);
+
+let nombreProp = "edad";
+
+console.log(gato[nombreProp]);
+
+//accediendo a un método
 gato.dormir();
+
+let perro = {
+  nombre: "Mochi",
+  edad: 1,
+  color: "blanco"
+}
+
+//desestructuración, que mientras sepamos los nombres de las propiedades de un objeto podremos usarlas para acceder a ellas
+let { color, edad } = perro;
+
+console.log(color, edad);
