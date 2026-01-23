7083e60e96af5077573d78d49143566ffc368a81
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-22 19:30:24 -0500

    avance

---
+++ 03-js/11-ecmascript.js
@@ -0,0 +1,25 @@
+var texto = "Hola!";
+
+var texto = "Adios!";
+
+console.log(texto);
+
+let nombre = "Jorge";
+
+nombre = "Jorge Osmar"
+
+// let nombre = "Victor";
+//RECOMENDACIÓN, NO USAR VAR EN NINGÚN CONTEXTO
+
+//Constante
+const IGV = 1.18;
+
+// IGV = 1.15;
+//No permite asignar un nuevo valor
+
+const frutas = ["Kiwi", "Naranja", "Piña"];
+
+//se permite modificar siempre y cuando la modificación venga de algún método interno
+frutas.push("Guayaba");
+
+console.log(frutas);
