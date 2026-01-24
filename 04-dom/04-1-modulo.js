c7974bcc3d212317e3c80b37e2f42a266ac65233
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-23 22:35:14 -0500

    avance

---
+++ 04-dom/04-1-modulo.js
@@ -0,0 +1,15 @@
+const saludar = (nombre) => {
+  return `Hola ${nombre}, como estás?`
+}
+
+const IGV = 1.18;
+
+const data = [
+  "React", "JS", "Babel", "Vite"
+]
+
+export {
+  saludar,
+  IGV,
+  data
+}
---
+++ 04-dom/04-app-modulo.js
@@ -0,0 +1,7 @@
+import { saludar, IGV, data } from "./04-1-modulo.js";
+
+console.log(saludar("Jorge"));
+
+console.log(`El precio total con IGV es: ${100 * IGV}`);
+
+console.log("hola!")
