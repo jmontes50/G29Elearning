d66eb4ccf83e048d5a5848e30e2defab4f73dcf4
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-23 22:42:12 -0500

    avance

--- 04-dom/04-1-modulo.js
+++ 04-dom/04-1-modulo.js
@@ -4,12 +4,15 @@ const saludar = (nombre) => {
 
 const IGV = 1.18;
 
+//supongamos data es lo más importante
 const data = [
   "React", "JS", "Babel", "Vite"
 ]
+//Si exportamos 01 sola cosa, usamos default
+export default data;
 
+//si son varias usamos las llaves y exportamos cada referencia por su nombre
 export {
   saludar,
-  IGV,
-  data
+  IGV
 }
--- 04-dom/04-app-modulo.js
+++ 04-dom/04-app-modulo.js
@@ -1,7 +1,12 @@
-import { saludar, IGV, data } from "./04-1-modulo.js";
+// import data from "./04-1-modulo.js"
+// import { saludar, IGV } from "./04-1-modulo.js";
+//cuando algo es exportado por default podemos cambiarle el nombre
+import datos, { saludar, IGV } from "./04-1-modulo.js";
 
 console.log(saludar("Jorge"));
 
 console.log(`El precio total con IGV es: ${100 * IGV}`);
 
 console.log("hola!")
+
+console.log(datos);
