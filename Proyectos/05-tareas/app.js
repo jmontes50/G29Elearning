5411e512ae5253be9d64e2dea4c9197522bff651
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-23 20:34:32 -0500

    avance

---
+++ Proyectos/05-tareas/app.js
@@ -0,0 +1,6 @@
+const titulo_tarea = document.getElementById("titulo-tarea");
+const desc_tarea = document.getElementById("descripcion-tarea");
+const btn_tarea = document.getElementById("btn-tarea");
+const lista_tareas = document.getElementById("lista-tareas");
+
+
