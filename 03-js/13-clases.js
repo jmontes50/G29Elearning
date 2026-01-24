2b6ee8e68a6a2d514e32ecce17db01d966cf6362
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-23 20:01:12 -0500

    avance

--- 03-js/13-clases.js
+++ 03-js/13-clases.js
@@ -11,19 +11,46 @@ console.log(usuario);
 //necesitamos que su nombre comience con mayúsculas
 //pensemos en una clase como una fábrica o un molde
 class User {
+  //indicar que propiedades tendrá el objeto
+  nombre;
+  direccion;
+  #email;
+  //dentro de la clase las propiedades privadas si son referenciables
+  #password; //al darle el numeral indicamos que es privado
   //estamos definiendo que propiedades tendrá la clase
-  constructor (Nombre, Direccion, Email, Password) {
+  constructor (Nombre, Direccion, Password) {
     //this.nombre = nombre //no hay problema en utilizar el mismo nombre/variable, se diferencian con this
     this.nombre = Nombre;
     this.direccion = Direccion;
-    this.email = Email;
-    this.password = Password;
+    // this.#email = Email;
+    this.#password = Password;
+  }
+  //getter - obtener / leer, va a ser un método/función que me permite obtener
+  get email() {
+    return this.#email;
+  }
+  //setter -
+  set email(nuevoEmail) {
+    //includes verifica si hay algo dentro de un string
+    if(!nuevoEmail.includes('@')){
+      throw new Error("El correo es inválido!!!");
+    }
+    this.#email = nuevoEmail;
   }
 }
 
 //instanciar, instancia
-const usuario2 = new User("Victor", "Av", "victor@codigo.edu.pe", "aqp");
-const usuario3 = new User("Jorge", "Av", "jorge@codigo.edu.pe", "ceviche");
+const usuario2 = new User("Victor", "Av", "aqp");
+const usuario3 = new User("Jorge", "Av", "ceviche");
 
 console.log(usuario2);
-console.log(usuario3);
+
+
+// usuario.password = "anticuchos";
+// console.log(usuario)
+
+// usuario3.#password = "adobo";
+
+usuario3.email = "j@gmail.com";
+
+console.log(usuario3.email);
