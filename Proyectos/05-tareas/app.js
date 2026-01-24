19101e0707dc80e9f971f83ba889ec35e5617423
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-23 20:45:09 -0500

    avance

--- Proyectos/05-tareas/app.js
+++ Proyectos/05-tareas/app.js
@@ -3,4 +3,22 @@ const desc_tarea = document.getElementById("descripcion-tarea");
 const btn_tarea = document.getElementById("btn-tarea");
 const lista_tareas = document.getElementById("lista-tareas");
 
+const arrTareas = [];
 
+btn_tarea.addEventListener("click", () => {
+  //el value es el valor que tiene un input en html
+  console.log(titulo_tarea.value);
+  console.log(desc_tarea.value);
+  //creamos un nuevo Obj con la info de la nueva tarea
+  const nuevaTarea = {
+    titulo: titulo_tarea.value,
+    descripcion: desc_tarea.value
+  }
+  //lo agregamos a un array
+  arrTareas.push(nuevaTarea);
+  console.table(arrTareas);
+
+  //limpiamos los input para escribir de nuevo
+  titulo_tarea.value = "";
+  desc_tarea.value = "";
+})
