165fc027ef621424f3797beb54c60f80976ed009
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-29 21:40:11 -0500

    avance

--- 05-asincronía/03-async-await.js
+++ 05-asincronía/03-async-await.js
@@ -24,10 +24,20 @@ const buscarNombre = async () => {
 //   console.log(error);
 // })
 
+const tieneCreditos = async () => {
+  // setTimeout(() => {
+  //   return "Si tiene créditos";
+  // }, 2000)
+  return "Si tiene créditos";
+
+}
+
 const ejecutar = async () => {
   try { //intenta esto
     const rpta = await buscarNombre(); //await lo que hace es esperar al resultado de la promesa
     console.log(rpta);
+    const rpta2 = await tieneCreditos()
+    console.log(rpta2);
   } catch (error) { //y si encuentras un error muestramelo
     console.log(error)
   }
