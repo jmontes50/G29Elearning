a933c5168f2f27a531da4658c6a60806499d8b94
Osmar Montesinos <osmarmontes50@gmail.com>
2026-01-30 22:35:51 -0500

    avance

---
+++ 06-node/01-node.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+console.log("Hola!!");
+
+const mostrarFecha = () => {
+  const ahora = new Date();
+  return ahora.toLocaleDateString();
+}
+
+console.log(mostrarFecha());
+// console.log(document);
+
+//axios tiene metodos con los nombres de cada tipo de verbo http (.get .post .put .delete)
+axios.get("https://697c2626889a1aecfeb18999.mockapi.io/usuarios")
+.then((rpta => {
+  // console.log(rpta); //en la rpta estan los headers y tambien el json de datos en una prop .data
+  console.log(rpta.data);
+}))
+.catch((error) => {
+  console.log(error);
+})
