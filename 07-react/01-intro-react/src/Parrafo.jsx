b6c4dd16708e7fab70f7713f7d074be969575d25
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-04 21:41:47 -0500

    avance

--- 07-react/01-intro-react/src/Parrafo.jsx
+++ 07-react/01-intro-react/src/Parrafo.jsx
@@ -1,12 +1,21 @@
+//Un fragment es un contenedor invisible que no se renderiza en el DOM.
+
 //Un componente es una función, y al ser una función puede recibir parámetros (props).
 const Parrafo = (props) => {
   //los props van a a llegar como un objeto.
-  console.log("props",props);
-  return <p>
-    Este es un Párrafo: {props.texto}
-    <br />
-    Este es el número: {props.numero}
-    </p>
-}
+  console.log("props", props);
+  return (
+    <>
+      <p>
+        Este es un Párrafo: {props.texto}
+        <br />
+        Este es el número: {props.numero}
+      </p>
+      <button onClick={() => props.accion(props.texto)}>
+        Saludar
+      </button>
+    </>
+  );
+};
 
 export default Parrafo;
