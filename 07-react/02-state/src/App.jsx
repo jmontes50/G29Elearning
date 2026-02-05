0a58f29123e3b9749cee0405cb61dc4a37de220d
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-04 22:25:49 -0500

    avance

--- 07-react/02-state/src/App.jsx
+++ 07-react/02-state/src/App.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 const App = () => {
+  //tiene que estar dentro del componente
+  // let numero = 10;
+  //const [valor, funcionActualizadora] = useState(valorInicial);
+  const [numero, setNumero] = useState(10);
+
+  const incrementar = () => {
+    //numero+=1;
+    setNumero(numero + 1);
+    console.log(numero);
+  }
+
   return (
-    <div>Hola</div>
+    <>
+      <div>{numero}</div>
+      <button onClick={incrementar}>
+        Incrementar
+      </button>
+    </>
   );
 }
 
