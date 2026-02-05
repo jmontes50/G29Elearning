a2f5f1d9c6a0076c5de89a194d203fc0f8420ec7
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-04 22:40:07 -0500

    avance

--- 07-react/02-state/src/App.jsx
+++ 07-react/02-state/src/App.jsx
@@ -5,11 +5,19 @@ const App = () => {
   // let numero = 10;
   //const [valor, funcionActualizadora] = useState(valorInicial);
   const [numero, setNumero] = useState(10);
+  //componentes controlados para los input
+  //amarrar el valor del input a un estado
+  const [valor, setValor] = useState("");
 
   const incrementar = () => {
     //numero+=1;
-    setNumero(numero + 1);
+    setNumero(numero + 1); //setter
     console.log(numero);
+  }
+
+  const manejarInput = (ev) => {
+    console.log(ev.target.value);
+    setValor(ev.target.value);
   }
 
   return (
@@ -18,6 +26,12 @@ const App = () => {
       <button onClick={incrementar}>
         Incrementar
       </button>
+      <hr/>
+      <input
+        type="text"
+        value={valor}
+        onChange={manejarInput}
+      />
     </>
   );
 }
