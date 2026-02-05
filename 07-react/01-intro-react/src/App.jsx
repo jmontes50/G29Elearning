85cf36a191ebc8c1a92bfbdd7bd9c3b6c8d17a85
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-04 20:08:54 -0500

    avance

---
+++ 07-react/01-intro-react/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div>Mi app</div>
+  )
+}
+
+export default App;
---
+++ 07-react/01-intro-react/src/main.jsx
@@ -0,0 +1,11 @@
+//Strict Mode es una característica de React que ayuda a identificar problemas potenciales en una aplicación.
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
