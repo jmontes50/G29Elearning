1fb7e774c821c9fe820c765d098ac5e351c511bf
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-05 20:10:54 -0500

    avance

--- 07-react/03-effect/src/App.jsx
+++ 07-react/03-effect/src/App.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-
   const [contador, setContador] = useState(10000);
+  const [interruptor, setInterruptor] = useState(false);
 
   const manejarClick = () => {
     setContador(contador + 1);
-  }
+  };
+
+  //cuando cambiamos algún estado, el componente entero vuelve a ejecutarse, pero con la información del estado ya actualizado.
+  console.log("Cambio!!", contador);
 
   return (
     <div>
       <h2>Contador: {contador}</h2>
-      <button onClick={manejarClick}>
-        Aumentar
-      </button>
+      <button onClick={manejarClick}>Aumentar</button>
+      <hr />
+      {/* renderizado condicional, utilizaremos operador ternario */}
+      <p>
+        Interruptor:{" "}
+        {interruptor ?
+        <span>Esta prendido</span> :
+        <span>Esta apagado</span>}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
