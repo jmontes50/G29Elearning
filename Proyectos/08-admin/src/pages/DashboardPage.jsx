7fed8f58ae1ec0213a3f3d0306f837014d827a6b
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-06 22:14:16 -0500

    avance

--- Proyectos/08-admin/src/pages/DashboardPage.jsx
+++ Proyectos/08-admin/src/pages/DashboardPage.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react"
+import { requestProducts } from "../services/productosService"
+
 const DashboardPage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsData = await requestProducts();
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error al obtener productos:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>DashboardPage</div>
   )
