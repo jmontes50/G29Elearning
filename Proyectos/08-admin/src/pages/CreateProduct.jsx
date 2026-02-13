b93b9b4960ff868bd21330c7c48297b87e9198d6
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-12 22:09:44 -0500

    avance

--- Proyectos/08-admin/src/pages/CreateProduct.jsx
+++ Proyectos/08-admin/src/pages/CreateProduct.jsx
@@ -1,17 +1,51 @@
-import { useState } from "react"
+import { useState } from "react";
+import Input from "../components/Input";
 
 const CreateProduct = () => {
   const [producto, SetProducto] = useState({
-    nombre:"",
-    descripcion:"",
-    precio:0,
-    stock:0,
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    stock: 0,
     // imagen:""
-  })
+  });
 
-  return (
-    <div>CreateProduct</div>
-  )
-}
+  const inputs = [
+    {
+      inputNombre: "Nombre",
+      tipo: "text",
+      placeholder: "Ingresa el nombre del producto",
+    },
+    {
+      inputNombre: "Descripción",
+      tipo: "text",
+      placeholder: "Ingrese descripción",
+    },
+    {
+      inputNombre: "Precio",
+      tipo: "number",
+      placeholder: "Ingresa el precii del producto",
+    },
+    {
+      inputNombre: "Stock",
+      tipo: "number",
+      placeholder: "Ingresa la cantidad almacenada",
+    },
+  ];
 
-export default CreateProduct
+  return <div>
+    <h2 className="text-3xl mb-4">Crear Producto</h2>
+    <div>
+      {inputs.map((input, index) => (
+        <Input
+          key={index}
+          inputNombre={input.inputNombre}
+          tipo={input.tipo}
+          placeholder={input.placeholder}
+         />
+      ))}
+    </div>
+  </div>;
+};
+
+export default CreateProduct;
