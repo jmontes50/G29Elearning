c26b9765f8e1bfd322aeed43d2743115f3f75699
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-12 21:13:58 -0500

    avance

--- Proyectos/08-admin/src/App.jsx
+++ Proyectos/08-admin/src/App.jsx
@@ -2,10 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage"
 import CreateProduct from "./pages/CreateProduct";
 
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     // Browser Router es un contexto que conecta los componente con la history de API de JS
     <BrowserRouter>
+    {/* cualquier componente que interactue con rutas necesita estar dentro de BrowserRouter */}
+      <Navbar />
       <Routes>
         {/* aquí adentro podemos definir las rutas */}
         <Route path="/" element={<DashboardPage />} />
---
+++ Proyectos/08-admin/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Store App</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Crear Producto</a></li>
+      {/* <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li> */}
+    </ul>
+  </div>
+</div>
+  )
+}
+
+export default Navbar
