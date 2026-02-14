12fdc85621acd02118719481190b1fcd8e17bc3b
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-13 20:57:59 -0500

    avance

--- Proyectos/08-admin/src/pages/UpdateProductPage.jsx
+++ Proyectos/08-admin/src/pages/UpdateProductPage.jsx
@@ -3,9 +3,56 @@ import { useParams } from 'react-router-dom';
 import { requestProductById } from '../services/productosService';
 
 const UpdateProductPage = () => {
+  const [producto, SetProducto] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    stock: 0,
+    // imagen:""
+  });
 
   const { id } = useParams();
   // console.log(id);
+
+  const inputs = [
+    {
+      inputNombre: "Nombre",
+      tipo: "text",
+      placeholder: "Ingresa el nombre del producto",
+      name:"nombre"
+    },
+    {
+      inputNombre: "Descripción",
+      tipo: "text",
+      placeholder: "Ingrese descripción",
+      name:"descripcion"
+    },
+    {
+      inputNombre: "Precio",
+      tipo: "number",
+      placeholder: "Ingresa el precio del producto",
+      name:"precio"
+    },
+    {
+      inputNombre: "Stock",
+      tipo: "number",
+      placeholder: "Ingresa la cantidad almacenada",
+      name: "stock"
+    },
+  ];
+
+  const manejarInputs = (evento ) => {
+    const name = evento.target.name;
+    const value = evento.target.value;
+    SetProducto({
+      ...producto, //copia del estado actual
+      [name]: value //actualiza la propiedad en base a name y value
+    })
+  }
+
+  const manejarSubmit = async (evento) => {
+    evento.preventDefault();
+  }
 
   useEffect(() => {
     const getProduct = async () => {
