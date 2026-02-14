2d614e88002c381682b9799bc8252b0af21eacf0
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-13 20:50:05 -0500

    avance

--- Proyectos/08-admin/src/pages/UpdateProductPage.jsx
+++ Proyectos/08-admin/src/pages/UpdateProductPage.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { requestProductById } from '../services/productosService';
 
 const UpdateProductPage = () => {
 
   const { id } = useParams();
-  console.log(id);
+  // console.log(id);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const producto = await requestProductById(id);
+        console.log(producto);
+      } catch (error) {
+        console.error("Error al obtener el producto:", error);
+      }
+    }
+    getProduct();
+  }, [id])
+
   return (
     <div>UpdateProductPage</div>
   )
