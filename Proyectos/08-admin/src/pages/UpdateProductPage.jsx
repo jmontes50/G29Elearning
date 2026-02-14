a61a59479b06c6dc81dc6c52f4170a2a80f6e594
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-13 21:03:35 -0500

    avance

--- Proyectos/08-admin/src/pages/UpdateProductPage.jsx
+++ Proyectos/08-admin/src/pages/UpdateProductPage.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import { requestProductById } from '../services/productosService';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { requestProductById } from "../services/productosService";
+import Input from "../components/Input";
 
 const UpdateProductPage = () => {
   const [producto, SetProducto] = useState({
@@ -19,40 +20,40 @@ const UpdateProductPage = () => {
       inputNombre: "Nombre",
       tipo: "text",
       placeholder: "Ingresa el nombre del producto",
-      name:"nombre"
+      name: "nombre",
     },
     {
       inputNombre: "Descripción",
       tipo: "text",
       placeholder: "Ingrese descripción",
-      name:"descripcion"
+      name: "descripcion",
     },
     {
       inputNombre: "Precio",
       tipo: "number",
       placeholder: "Ingresa el precio del producto",
-      name:"precio"
+      name: "precio",
     },
     {
       inputNombre: "Stock",
       tipo: "number",
       placeholder: "Ingresa la cantidad almacenada",
-      name: "stock"
+      name: "stock",
     },
   ];
 
-  const manejarInputs = (evento ) => {
+  const manejarInputs = (evento) => {
     const name = evento.target.name;
     const value = evento.target.value;
     SetProducto({
       ...producto, //copia del estado actual
-      [name]: value //actualiza la propiedad en base a name y value
-    })
-  }
+      [name]: value, //actualiza la propiedad en base a name y value
+    });
+  };
 
   const manejarSubmit = async (evento) => {
     evento.preventDefault();
-  }
+  };
 
   useEffect(() => {
     const getProduct = async () => {
@@ -62,13 +63,32 @@ const UpdateProductPage = () => {
       } catch (error) {
         console.error("Error al obtener el producto:", error);
       }
-    }
+    };
     getProduct();
-  }, [id])
+  }, [id]);
 
   return (
-    <div>UpdateProductPage</div>
-  )
-}
+    <div>
+      <h2 className="text-3xl mb-4">Actualizar Producto</h2>
+      <form onSubmit={manejarSubmit}>
+        {inputs.map((input, index) => (
+          <Input
+            key={index}
+            inputNombre={input.inputNombre}
+            tipo={input.tipo}
+            placeholder={input.placeholder}
+            name={input.name}
+            // props para componentes controlados
+            value={producto}
+            manejarInputs={manejarInputs}
+          />
+        ))}
+        <button type="submit" className="btn btn-primary">
+          Guardar
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default UpdateProductPage
+export default UpdateProductPage;
