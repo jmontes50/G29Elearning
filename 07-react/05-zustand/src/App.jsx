a4700aa64a3965f7cd33bccd05213ae40a80af1b
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-19 20:24:49 -0500

    avance

--- 07-react/05-zustand/src/App.jsx
+++ 07-react/05-zustand/src/App.jsx
@@ -2,10 +2,10 @@ import useContadorStore from './store';
 
 const App = () => {
 
-  // console.log(useContadorStore());
+  console.log(useContadorStore());
   //useContadorStore es una función, y al ejecutarla nos devuelve el estado del store (un objeto)
   //al ser un objeto lo podemos desestructurar para obtener lo que queremos
-  const { contador, subirAMil, incrementar } = useContadorStore();
+  const { contador, subirAMil, incrementar, frutas } = useContadorStore();
 
   return (
     <div>
--- 07-react/05-zustand/src/store.js
+++ 07-react/05-zustand/src/store.js
@@ -3,10 +3,12 @@ import { create } from 'zustand';
 //necesitamos que tenga el prefijo us
 //create va a pedir como argumento una función
 //y esa función tiene que retornar un objeto
+//set es es una funcion que permite actualizar el estado del store y tenemos que indicar cual es el cambio que queremos hacer, es decir, que propiedad del estado queremos actualizar y con que valor
 const useContadorStore = create((set) => {
   return {
     //tenemos total flexibilidad para definir que almacenamos
     contador: 0, //definimos que queremos guardar en este store
+    frutas: ['manzana', 'banana', 'naranja'], //podemos guardar cualquier tipo de dato, incluso objetos o arrays
     //podemos definir que acciones queremos ejecutar,
     subirAMil: () => set({ contador: 1000 }),
     incrementar: () => set((state) => ({ contador: state.contador + 1}))
